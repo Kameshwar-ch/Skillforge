4be008781af7c10998c514ea36c8f8246261870d
Himanshu Barad <Himanshu.Barad@cognizant.com>
2026-03-27 15:21:07 +0530

    Removed Seeding Data

--- Skillforge/Program.cs
+++ Skillforge/Program.cs
@@ -17,22 +17,6 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IForgotPasswordService, ForgotPasswordService>();
 
 var app = builder.Build();
-using (var scope = app.Services.CreateScope())
-{
-    var context = scope.ServiceProvider.GetRequiredService<SkillForgeDB>();
-    if (!context.Users.Any())
-    {
-        context.Users.AddRange(
-            new User { Name = "Rahul Kumar", Role = UserRole.Admin, Email = "rahul@skillforge.com", Phone = "9876543210", Status = true, Password = BCrypt.Net.BCrypt.HashPassword("Admin@123") },
-            new User { Name = "Priya Sharma", Role = UserRole.Manager, Email = "priya@skillforge.com", Phone = "9876543211", Status = true, Password = BCrypt.Net.BCrypt.HashPassword("Manager@123") },
-            new User { Name = "Amit Singh", Role = UserRole.Trainer, Email = "amit@skillforge.com", Phone = "9876543212", Status = true, Password = BCrypt.Net.BCrypt.HashPassword("Trainer@123") },
-            new User { Name = "Sneha Patel", Role = UserRole.Employee, Email = "sneha@skillforge.com", Phone = "9876543213", Status = true, Password = BCrypt.Net.BCrypt.HashPassword("Employee@123") },
-            new User { Name = "Deepak Verma", Role = UserRole.HR, Email = "deepak@skillforge.com", Phone = "9876543214", Status = true, Password = BCrypt.Net.BCrypt.HashPassword("Hr@12345") }
-        );
-        context.SaveChanges();
-        Console.WriteLine("Seed data added!");
-    }
-}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
