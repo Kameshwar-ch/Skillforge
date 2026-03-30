c03ebbedc2c1ed9eda1e7757272f17a0db089b8d
Himanshu Barad <Himanshu.Barad@cognizant.com>
2026-03-30 09:42:56 +0530

    Created Separate class file for each DTO

--- Skillforge/Dto/ForgotPasswordDtos.cs
+++ Skillforge/Dto/ForgotPasswordDtos.cs
@@ -7,27 +7,3 @@ public class ForgotPasswordRequestDto
 }
 
 // This DTO is used to take the response from user and update the database
-public class ResetPasswordDto
-{
-    public string Email { get; set; } = string.Empty;
-    public string NewPassword { get; set; } = string.Empty;
-    public string ConfirmPassword { get; set; } = string.Empty;
-}
-
-public class ApiResponseDto
-{
-    public bool Success { get; set; }
-    public string Message { get; set; } = string.Empty;
-
-    // This will just return the object of DTO with required Result type
-    public static ApiResponseDto SuccessResponse(string message)
-    {
-        return new ApiResponseDto { Success = true, Message = message};
-    }
-
-    public static ApiResponseDto FailResponse(string message)
-    {
-        return new ApiResponseDto {Success = false, Message = message};
-    }
-}
-
---
+++ Skillforge/Dto/ResetPasswordDto.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Skillforge.Dto;
+
+public class ResetPasswordDto
+{
+    public string Email { get; set; } = string.Empty;
+    public string NewPassword { get; set; } = string.Empty;
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
