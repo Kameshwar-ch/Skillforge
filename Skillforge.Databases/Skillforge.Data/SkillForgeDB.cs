c8cc45a89f9ffb26f4489d724d78869dd02cd66d
Himanshu Barad <Himanshu.Barad@cognizant.com>
2026-03-30 09:52:54 +0530

    Removed the connection string

--- Skillforge.Databases/Skillforge.Data/SkillForgeDB.cs
+++ Skillforge.Databases/Skillforge.Data/SkillForgeDB.cs
@@ -30,8 +30,7 @@ public class SkillForgeDB : DbContext
     public virtual DbSet<Assessment> Assessments { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("Data Source=LTIN719061\\SQLEXPRESS; Initial Catalog=newMouseDb;Integrated Security=True;TrustServerCertificate=True")
-            .ConfigureWarnings(w => w.Ignore(Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.PendingModelChangesWarning));
+        
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
