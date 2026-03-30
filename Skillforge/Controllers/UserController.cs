9bd910bb686d3569cbf9dc9fb02f91558eb22996
Himanshu Barad <Himanshu.Barad@cognizant.com>
2026-03-30 09:50:37 +0530

    Added api/User

--- Skillforge/Controllers/UserController.cs
+++ Skillforge/Controllers/UserController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Skillforge.Controllers
 {
-    [Route("/[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
     {
