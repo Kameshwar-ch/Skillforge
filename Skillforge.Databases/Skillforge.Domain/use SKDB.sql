b57d3225c58e5209bd5b55ee5b1206fa6beb613f
Vaishnavi <vaishnavipirangi@gmail.com>
2026-04-01 09:18:47 +0530

    commited

---
+++ Skillforge.Databases/Skillforge.Domain/use SKDB.sql
@@ -0,0 +1,3 @@
+use SKDB
+
+Insert into [User] values('judy','Employee','vaish@gmail.com','hashpass','saltpass','1234567899',1)
--- Skillforge/Program.cs
+++ Skillforge/Program.cs
@@ -20,8 +20,6 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-
-
 }
 
 app.UseHttpsRedirection();
