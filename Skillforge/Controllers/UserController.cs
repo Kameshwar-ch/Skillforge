bce4fa05bc2426fa785dabd4efb13ef6dc5711c7
VaishnaviPirangi04 <Vaishnavi.Pirangi@cognizant.com>
2026-04-02 12:33:51 +0530

    Refactor DeleteUser method in UserController

--- Skillforge/Controllers/UserController.cs
+++ Skillforge/Controllers/UserController.cs
@@ -87,31 +87,31 @@ namespace Skillforge.Controller
 				return StatusCode(500, new { message = ex.Message });
 			}
 		}
-         [HttpDelete("{userId}")]
+        [HttpDelete("{userId}")]
         [Authorize(Roles = nameof(UserRole.Admin))]
-    public async Task<IActionResult> DeleteUser(int userId)
-   {
-    try
-    {
-        if (userId <= 0)
-        {
-            return BadRequest("Invalid User ID");
-        }
+    	public async Task<IActionResult> DeleteUser(int userId)
+   		{
+    		try
+    		{
+        		if (userId <= 0)
+        		{
+            		return BadRequest("Invalid User ID");
+        		}
 
-        var deleted = await _userService.DeleteUser(userId);
+        		var deleted = await _userService.DeleteUser(userId);
 
-        if (!deleted)
-        {
-            return NotFound(DeleteUserMessages.Delete.NotFound);
-        }
+        		if (!deleted)
+        		{
+            		return NotFound(DeleteUserMessages.Delete.NotFound);
+        		}
 
-        return Ok(DeleteUserMessages.Delete.Success);
-    }
-    catch (Exception ex)
-    {
-        return StatusCode(500,ex.Message);
-    }
-}
+        		return Ok(DeleteUserMessages.Delete.Success);
+    		}
+    		catch (Exception ex)
+    		{
+        		return StatusCode(500,ex.Message);
+    		}
+		}
 
     }
 }
