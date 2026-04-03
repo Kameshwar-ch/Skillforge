7018a4ceab8a05ba543893b3c3cb0c2b0e62c8bf
Sabarinathan P <sabarinathanspace25@gmail.com>
2026-04-03 09:34:16 +0530

    Implemented soft delete for user

--- Skillforge/Repository/UserRepository.cs
+++ Skillforge/Repository/UserRepository.cs
@@ -42,7 +42,8 @@ public class UserRepository : IUserRepository
                 return false;
             }
 
-            context.Users.Remove(user);
+           user.Status=false;
+           context.Users.Update(user);
             await context.SaveChangesAsync();
 
             Console.WriteLine("DELETE SUCCESS");
