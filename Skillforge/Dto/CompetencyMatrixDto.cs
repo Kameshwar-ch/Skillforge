2bb89bb1f23b6aab143d864f33f238b00ca18c25
Kameshwar <Kameshwar.Chankapure@cognizant.com>
2026-04-07 17:02:26 +0530

    added requried dto's

---
+++ Skillforge/Dto/CompetencyMatrixDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Skillforge.Dto
+{
+	public class CompetencyMatrixDto
+	{
+		public int EmployeeId { get; set; }
+
+		[Required]
+		public string? EmployeeName { get; set; }
+		public List<EmployeeSkillDto> Skills { get; set; } = new();
+	}
+}
---
+++ Skillforge/Dto/EmployeeSkillDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Skillforge.Dto
+{
+	public class EmployeeSkillDto
+	{
+		[Required]
+		public string? SkillName { get; set; }
+		[Required]
+		public string? Level { get; set; }
+	}
+}
