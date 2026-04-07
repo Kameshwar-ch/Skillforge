8170c5cb4b9af9e325126d3d09c389a9da44c051
Kameshwar <Kameshwar.Chankapure@cognizant.com>
2026-04-07 17:03:46 +0530

    create competencyrepository along with competencyInterface

---
+++ Skillforge/Repository/CompetencyRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Skillforge.Data;
+using Skillforge.Domain;
+
+namespace Skillforge.Repository
+{
+	public class CompetencyRepository : ICompetencyRepository
+	{
+		private readonly SkillForgeDB context;
+		public CompetencyRepository(SkillForgeDB context)
+		{
+			this.context = context;
+		}
+		public async Task<List<User>> GetEmployeesWithSkillsAsync()
+		{
+			return await context.Users
+					.Where(u => u.Role == UserRole.Employee)
+					.Include(sk => sk.SkillGaps)
+					.ThenInclude(c => c.Competency)
+					.ToListAsync();
+
+		}
+
+	}
+}
