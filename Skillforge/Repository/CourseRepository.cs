8dc1508bae3944600cedb55083ed335098455d93
Kameshwar <Kameshwar.Chankapure@cognizant.com>
2026-04-20 14:31:59 +0530

    Added CourseRepository along with ICourseRepository

---
+++ Skillforge/Repository/CourseRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Skillforge.Data;
+using Skillforge.Domain;
+
+namespace Skillforge.Repository
+{
+	public class CourseRepository : ICourseRepository
+	{
+		private readonly SkillForgeDB _context;
+		public CourseRepository(SkillForgeDB context)
+		{
+			_context = context;
+		}
+		public async Task<Course?> GetCourseByIdAsync(int courseId)
+		{
+			return await _context.Courses.FindAsync(courseId);
+		}
+
+		public async Task<int> AddModuleAsync(Module module)
+		{
+			_context.Modules.Add(module);
+			await _context.SaveChangesAsync();
+			return module.ModuleID;
+		}
+
+	}
+}
---
+++ Skillforge/Repository/ICourseRepository.cs
@@ -0,0 +1,10 @@
+﻿using Skillforge.Domain;
+
+namespace Skillforge.Repository
+{
+	public interface ICourseRepository
+	{
+		Task<Course?> GetCourseByIdAsync(int courseId);
+		Task<int> AddModuleAsync(Module module);
+	}
+}
