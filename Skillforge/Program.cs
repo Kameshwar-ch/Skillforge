dc95bf1b49d08d99fab16582c2c3f2f0cf592f94
Raunak_Ritesh_Jijotia <RaunakRitesh.Jijotia@cognizant.com>
2026-04-27 10:12:35 +0530

    Update: Updated the Program.cs file with new Dependencies

--- Skillforge/Program.cs
+++ Skillforge/Program.cs
@@ -50,6 +50,7 @@ builder.Services.AddScoped<IAuditLogService, AuditLogService>();
 builder.Services.AddScoped<IAuditLogRepository, AuditLogRepository>();
 builder.Services.AddScoped<IAuditLogService, AuditLogService>();
 builder.Services.AddScoped<ICertificationRepository, CertificationRepository>();
+builder.Services.AddScoped<INotificationRepository, NotificationRepository>();
 builder.Services.AddScoped<INotificationService, NotificationService>();
 builder.Services.AddScoped<ICertificationService, CertificationService>();
 builder.Services.AddScoped<ICourseRepository, CourseRepository>();
@@ -63,9 +64,10 @@ builder.Services.AddScoped<IComplianceRecordService, ComplianceRecordService>();
 builder.Services.AddScoped<IReportRepository, ReportRepository>();
 builder.Services.AddScoped<IReportService, ReportService>();
 builder.Services.AddHostedService<ReportSchedulerBackgroundService>();
+builder.Services.AddHostedService<NotificationSchedulerBackgroundService>();
 
 var secretKey = Environment.GetEnvironmentVariable("JWT_SECRET_KEY");
-if (secretKey == null)
+if (secretKey is null)
 {
     throw new Exception("Secret key is NUll");
 }
