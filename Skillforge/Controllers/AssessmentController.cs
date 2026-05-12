c3878e6af1371b357826ae186d273e76c53f8af5
JashwanthReddy <jashwanthreddy927@gmail.com>
2026-05-12 17:37:37 +0530

    added authentication

--- Skillforge/Controllers/AssessmentController.cs
+++ Skillforge/Controllers/AssessmentController.cs
@@ -141,7 +141,7 @@ public class AssessmentController : ControllerBase
     }
 
     [HttpGet("get-assessment/{assessmentId}")]
-    [Authorize]
+    [Authorize(Roles = nameof(UserRole.Trainer))]
     public async Task<IActionResult> GetAssessmentById(int assessmentId)
     {
         var result = await _assessmentService.GetAssessmentByIdAsync(assessmentId);
